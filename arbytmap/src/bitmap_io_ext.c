05304e75260f526171174b3668c017195ee1a7af
Moses <29311834+MosesofEgypt@users.noreply.github.com>
2017-02-03 02:30:03 -0500

    Implemented C function for padding and unpadding 24 and 48 bit textures Finished implementing functionality to pack to all raw formats Fixed bugs with saving images to files Changed it so you cant load formats with > 8bpp if deep color mode isnt set Replaced a lot of instance of [0]*array_length with bytearray(array_length)     to speed the system up and save RAM Still cleaning stuff up(this will go on for a while) Misc tweaks and fixes

--- arbytmap/src/bitmap_io_ext.c
+++ arbytmap/src/bitmap_io_ext.c
@@ -6,51 +6,186 @@
 #include "object.h"
 
 
-static PyObject *py_swizzle_array(PyObject *self, PyObject *args) {
-    Py_buffer bufs[6];
+static void pad_24bit_array(
+    Py_buffer *padded_pix_buf, Py_buffer *unpadded_pix_buf)
+{
+    unsigned long long i=0, j=0, max_i=0;
 
-    // Get the pointers to each of the array objects
-    if (!PyArg_ParseTuple(args, "w*w*w*w*w*w*:swizzle_array",
-        &bufs[0], &bufs[1], &bufs[2], &bufs[3], &bufs[4], &bufs[5]))
-        return Py_None;
+    unsigned long (*padded_pix)[];
+    unsigned char (*unpadded_pix)[];
 
-    swizzle_char_array(
-        bufs[0].len, bufs[0].buf, bufs[1].len, bufs[1].buf,
-        bufs[2].len, bufs[2].buf, bufs[3].len, bufs[3].buf,
-        bufs[4].len, bufs[4].buf, bufs[5].len, bufs[5].buf,
-        bufs[5].itemsize, 1);
+    padded_pix   = (unsigned long(*)[]) padded_pix_buf->buf;
+    unpadded_pix = (unsigned char(*)[]) unpadded_pix_buf->buf;
 
-    // Release the buffer objects
-    PyBuffer_Release(&bufs[0]);
-    PyBuffer_Release(&bufs[1]);
-    PyBuffer_Release(&bufs[2]);
-    PyBuffer_Release(&bufs[3]);
-    PyBuffer_Release(&bufs[4]);
-    PyBuffer_Release(&bufs[5]);
+    max_i = (padded_pix_buf->len)/4;
 
-    /* Convert from a C integer value to a Python integer instance */
-    return Py_None;
+    for (i=0; i < max_i; i++) {
+        j = i*3;
+        (*padded_pix)[i] = ((*unpadded_pix)[j] +
+                           ((*unpadded_pix)[j+1]<<8) +
+                           ((*unpadded_pix)[j+2]<<16));
+    }
 }
 
-static PyObject *py_unswizzle_array(PyObject *self, PyObject *args) {
+static void unpad_24bit_array(
+    Py_buffer *unpadded_pix_buf, Py_buffer *padded_pix_buf)
+{
+    unsigned long long i=0, j=0, k=0, max_i=0;
+    unsigned long pixel=0;
+
+    unsigned char (*unpadded_pix)[];
+    unsigned char (*padded_pix_8)[];
+    unsigned long (*padded_pix_32)[];
+
+    unpadded_pix  = (unsigned char(*)[]) unpadded_pix_buf->buf;
+    padded_pix_8  = (unsigned char(*)[]) padded_pix_buf->buf;
+    padded_pix_32 = (unsigned long(*)[]) padded_pix_buf->buf;
+
+    max_i = (unpadded_pix_buf->len)/3;
+
+    if ( padded_pix_buf->itemsize == 4) {
+        for (i=0; i < max_i; i++) {
+            j=i*3;
+            pixel = (*padded_pix_32)[i];
+            (*unpadded_pix)[j] = pixel&0xFF; j++;
+            (*unpadded_pix)[j] = (pixel>>8)&0xFF; j++;
+            (*unpadded_pix)[j] = (pixel>>16)&0xFF;
+        }
+    } else {
+        for (i=0; i < max_i; i++) {
+            /*
+            If the itemsize isnt 4, the pixels havent been
+            packed, and since the channel order in Arbytmap
+            is ARGB, we need to skip each pixels first byte
+            */
+            j=i*3; k=i*4 + 1;
+            (*unpadded_pix)[j] = (*padded_pix_8)[k]; j++; k++;
+            (*unpadded_pix)[j] = (*padded_pix_8)[k]; j++; k++;
+            (*unpadded_pix)[j] = (*padded_pix_8)[k];
+        }
+    }
+}
+
+static void pad_48bit_array(
+    Py_buffer *padded_pix_buf, Py_buffer *unpadded_pix_buf)
+{
+    unsigned long long i=0, j=0, max_i=0;
+
+    unsigned long long (*padded_pix)[];
+    unsigned short (*unpadded_pix)[];
+
+    padded_pix   = (unsigned long long(*)[]) padded_pix_buf->buf;
+    unpadded_pix = (unsigned short(*)[]) unpadded_pix_buf->buf;
+
+    max_i = (padded_pix_buf->len)/8;
+
+    for (i=0; i < max_i; i++) {
+        j = i*3;
+        (*padded_pix)[i] = ((unsigned long long)(*unpadded_pix)[j] +
+                           ((unsigned long long)(*unpadded_pix)[j+1]<<16) +
+                           ((unsigned long long)(*unpadded_pix)[j+2]<<32));
+    }
+}
+
+static void unpad_48bit_array(
+    Py_buffer *unpadded_pix_buf, Py_buffer *padded_pix_buf)
+{
+    unsigned long long i=0, j=0, k=0, max_i=0, pixel=0;
+
+    unsigned short (*unpadded_pix)[];
+    unsigned short (*padded_pix_16)[];
+    unsigned long long(*padded_pix_64)[];
+
+    unpadded_pix  = (unsigned short(*)[]) unpadded_pix_buf->buf;
+    padded_pix_16 = (unsigned short(*)[]) padded_pix_buf->buf;
+    padded_pix_64 = (unsigned long long(*)[]) padded_pix_buf->buf;
+
+    max_i = (unpadded_pix_buf->len)/6;
+
+    if ( padded_pix_buf->itemsize == 8) {
+        for (i=0; i < max_i; i++) {
+            j=i*3;
+            pixel = (*padded_pix_64)[i];
+            (*unpadded_pix)[j] = pixel&0xFFff; j++;
+            (*unpadded_pix)[j] = (pixel>>16)&0xFFff; j++;
+            (*unpadded_pix)[j] = (pixel>>32)&0xFFff;
+        }
+    } else {
+        for (i=0; i < max_i; i++) {
+            /*
+            If the itemsize isnt 8, the pixels havent been
+            packed, and since the channel order in Arbytmap
+            is ARGB, we need to skip each pixels first 2 bytes
+            */
+            j=i*3; k=i*4 + 1;
+            (*unpadded_pix)[j] = (*padded_pix_16)[k]; j++; k++;
+            (*unpadded_pix)[j] = (*padded_pix_16)[k]; j++; k++;
+            (*unpadded_pix)[j] = (*padded_pix_16)[k];
+        }
+    }
+}
+
+static PyObject *py_pad_24bit_array(PyObject *self, PyObject *args) {
     Py_buffer bufs[2];
 
     // Get the pointers to each of the array objects
-    if (!PyArg_ParseTuple(args, "w*w*w*w*w*w*:swizzle_array",
-        &bufs[0], &bufs[1], &bufs[2], &bufs[3], &bufs[4], &bufs[5]))
+    if (!PyArg_ParseTuple(args, "w*w*:pad_24bit_array", &bufs[0], &bufs[1]))
         return Py_None;
 
-    swizzle_char_array(
-        bufs[0].len, bufs[0].buf, bufs[1].len, bufs[1].buf,
-        bufs[2].len, bufs[2].buf, bufs[3].len, bufs[3].buf,
-        bufs[4].len, bufs[4].buf, bufs[5].len, bufs[5].buf,
-        bufs[5].itemsize, 0);
+    pad_24bit_array(&bufs[0], &bufs[1]);
 
     // Release the buffer objects
     PyBuffer_Release(&bufs[0]);
     PyBuffer_Release(&bufs[1]);
 
-    /* Convert from a C integer value to a Python integer instance */
+    return Py_None;
+}
+
+static PyObject *py_pad_48bit_array(PyObject *self, PyObject *args) {
+    Py_buffer bufs[2];
+
+    // Get the pointers to each of the array objects
+    if (!PyArg_ParseTuple(args, "w*w*:pad_48bit_array", &bufs[0], &bufs[1]))
+        return Py_None;
+
+    pad_48bit_array(&bufs[0], &bufs[1]);
+
+    // Release the buffer objects
+    PyBuffer_Release(&bufs[0]);
+    PyBuffer_Release(&bufs[1]);
+
+    return Py_None;
+}
+
+static PyObject *py_unpad_24bit_array(PyObject *self, PyObject *args) {
+    Py_buffer bufs[2];
+
+    // Get the pointers to each of the array objects
+    if (!PyArg_ParseTuple(args, "w*w*:unpad_24bit_array", &bufs[0], &bufs[1]))
+        return Py_None;
+
+    unpad_24bit_array(&bufs[0], &bufs[1]);
+
+    // Release the buffer objects
+    PyBuffer_Release(&bufs[0]);
+    PyBuffer_Release(&bufs[1]);
+
+    return Py_None;
+}
+
+static PyObject *py_unpad_48bit_array(PyObject *self, PyObject *args) {
+    Py_buffer bufs[2];
+
+    // Get the pointers to each of the array objects
+    if (!PyArg_ParseTuple(args, "w*w*:unpad_48bit_array", &bufs[0], &bufs[1]))
+        return Py_None;
+
+    unpad_48bit_array(&bufs[0], &bufs[1]);
+
+    // Release the buffer objects
+    PyBuffer_Release(&bufs[0]);
+    PyBuffer_Release(&bufs[1]);
+
     return Py_None;
 }
 
@@ -58,10 +193,10 @@ static PyObject *py_unswizzle_array(PyObject *self, PyObject *args) {
 "METH_VARGS" tells Python how to call the handler.
 The {NULL, NULL} entry indicates the end of the method definitions */
 static PyMethodDef bitmap_io_ext_methods[] = {
-    //{"pad_24bit_array", py_pad_24bit_array, METH_VARARGS, ""},
-    //{"pad_48bit_array", py_pad_48bit_array, METH_VARARGS, ""},
-    //{"unpad_24bit_array", py_unpad_24bit_array, METH_VARARGS, ""},
-    //{"unpad_48bit_array", py_unpad_48bit_array, METH_VARARGS, ""},
+    {"pad_24bit_array", py_pad_24bit_array, METH_VARARGS, ""},
+    {"pad_48bit_array", py_pad_48bit_array, METH_VARARGS, ""},
+    {"unpad_24bit_array", py_unpad_24bit_array, METH_VARARGS, ""},
+    {"unpad_48bit_array", py_unpad_48bit_array, METH_VARARGS, ""},
     //{"uncompress_rle", py_uncompress_rle, METH_VARARGS, ""},
     {NULL, NULL, 0, NULL}      /* sentinel */
 };
