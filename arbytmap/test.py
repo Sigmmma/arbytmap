3a846a72df39b3bbc886d24f911378443ddc681a
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-12-18 13:48:09 -0500

    getting closer

--- arbytmap/test.py
+++ arbytmap/test.py
@@ -120,7 +120,7 @@ def run_test(print_formats=False, deep_color=False,
     
 if __name__ == "__main__":
     try:
-        run_test(print_formats=False, deep_color=False,
+        run_test(print_formats=False, deep_color=True,
                  keep_alpha=False, ck_trans=False)
     except:
         print(format_exc())
